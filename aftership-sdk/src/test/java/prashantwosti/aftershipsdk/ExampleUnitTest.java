b2c5c0ec2228cf57126999b45141eceeb3f276c5
prashantwosti <prashantwosti@gmail.com>
2016-05-14 11:38:49 +0545

    test

--- aftership-sdk/src/test/java/prashantwosti/aftershipsdk/ExampleUnitTest.java
+++ aftership-sdk/src/test/java/prashantwosti/aftershipsdk/ExampleUnitTest.java
@@ -1,15 +1,69 @@
 package prashantwosti.aftershipsdk;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import prashantwosti.aftershipsdk.tracking.AfterShipAPI;
+import prashantwosti.aftershipsdk.tracking.createtracking.CreateTrackingResponse;
+import prashantwosti.aftershipsdk.tracking.createtracking.TrackingRequest;
+import prashantwosti.aftershipsdk.tracking.singletracking.TrackingDetailData;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+@RunWith(MockitoJUnitRunner.class)
+public class ExampleUnitTest extends TestCase {
+
+    AfterShip afterShip;
+
+    @Mock
+    AfterShipAPI api;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        afterShip = new AfterShip("e624c6d3-1c27-4f16-9be9-70712cdbacab");
     }
+
+    @Test
+    public void shouldPrintResponseFromSingleTrackingApi() throws Exception {
+        Assert.assertNotNull(afterShip);
+        afterShip.getSingleTrackingResult("sf-express", "070630669263",
+                new AfterShip.OnSingleTrackingResultCallback() {
+            @Override
+            public void onSuccess(TrackingDetailData response) {
+                Assert.assertNotNull(response.getTracking());
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+    }
+
+    @Test
+    public void shouldPrintResponseFromCreateTrackingApi() throws Exception {
+        afterShip.createTracking(new TrackingRequest.TrackingData("070630669263"),
+                new AfterShip.OnCreateTrackingCallback() {
+            @Override
+            public void onSuccess(CreateTrackingResponse.Data response) {
+                Assert.assertNotNull(response.getTracking());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Assert.fail(t.toString());
+            }
+        });
+    }
+
 }
